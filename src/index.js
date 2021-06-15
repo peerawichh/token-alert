83ce1e1ec86f6d631f3c11cf11777c7504f2fcc8
Peerawich Sukijsopon <peerawich@Peerawichs-MacBook-Pro.local>
2021-06-15 12:50:22 +0700

    modified:   src/config.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const tendermint = require('./tendermint');
 const config = require('./config');
 const notifty = require('./notify');
 const cron = require('node-cron');
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('path');
 
-const CRON_TAB = `${config.CRON_MINUTE} ${config.CRON_HOUR} ${config.CRON_DAY_OF_MONTH} ${config.CRON_MONTH} ${config.CRON_DAY_OF_WEEK}`;
-cron.schedule(CRON_TAB, checkNodeToken);
+const CRON_TAB_1 = `${config.CRON_MINUTE} ${config.CRON_HOUR} ${config.CRON_DAY_OF_MONTH} ${config.CRON_MONTH} ${config.CRON_DAY_OF_WEEK}`;
+const CRON_TAB_2 = `5 ${config.CRON_MINUTE} ${config.CRON_HOUR.split(',')[1]} ${config.CRON_DAY_OF_MONTH} ${config.CRON_MONTH} ${config.CRON_DAY_OF_WEEK}`
+cron.schedule(CRON_TAB_1, checkNodeToken);
+cron.schedule(CRON_TAB_2, checkUnconditionalNodeToken);
 
 async function checkNodeToken() {
     console.log('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
@@ -14,24 +19,81 @@ async function checkNodeToken() {
     try {
         let node_id_list = await getNodeIDByRole('');
         let queriedNodeIDList = await queryNodeByTokenAmount(node_id_list);
-        node_id_list = null;
 
-        if (queriedNodeIDList.length === config.UNCONDITIONAL_NODE_LIST.split(',').length){
+        if (queriedNodeIDList.length === 0) {
             console.log(`No nodes with token amount below the threshold (${config.TOKEN_THRESHOLD_TO_ALERT}) were detected\n`)
             await notifty.lineNotify(`No nodes with token amount below the threshold (${config.TOKEN_THRESHOLD_TO_ALERT}) were detected`);
+            return;
         }
 
         const nodeListGroupedByMarketingName = await groupNodesByMarketingName(queriedNodeIDList);
         const Orgs = Object.keys(nodeListGroupedByMarketingName);
 
-        Orgs.forEach( Org => {
+        Orgs.forEach(Org => {
             let nodeList = nodeListGroupedByMarketingName[Org];
             let message = `${Org}`;
-            nodeList.forEach( node => {
-                message = message.concat(`\n\n${node.node_id} (${node.role}): ${node.token} ${node.token>1 ? 'tokens': 'token'} left`);
+            nodeList.forEach(node => {
+                message = message.concat(`\n\n${node.node_id} (${node.role}): ${node.token} ${node.token > 1 ? 'tokens' : 'token'} left`);
             });
-            console.log(`Notifying low token amount of ${Org}: ${nodeList.length} ${nodeList.length>1 ? 'nodes' : 'node'} in total\n`);
+            console.log(`Notifying low token amount of ${Org}: ${nodeList.length} ${nodeList.length > 1 ? 'nodes' : 'node'} in total\n`);
             notifty.lineNotify(message);
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function checkUnconditionalNodeToken() {
+
+    try {
+
+        let node_id_list = config.UNCONDITIONAL_NODE_LIST.split(',');
+        node_id_list = node_id_list.map(async (node) => {
+
+            const result = await tendermint.query('GetNodeToken', { node_id: node });
+            const node_info = await getNodeInfo(node);
+
+            return {
+                node_id: node,
+                token: result.amount,
+                node_name: node_info.marketing_name_en,
+                role: node_info.role
+            }
+
+        });
+
+        node_id_list = await Promise.all(node_id_list);
+        node_id_list = node_id_list.filter(node => typeof node !== 'undefined');;
+
+        node_id_list.forEach(async (node) => {
+
+            const fileName = `${node.node_id}-latest-token-amount`
+            const filePath = path.join(__dirname, '..', 'data', fileName);
+            let latestTokenAmount;
+
+            try {
+                latestTokenAmount = fs.readFileSync(filePath);
+
+            } catch (err) {
+                
+            }
+
+            let message = `${node.node_name}\n\n${node.node_id} (${node.role}): ${node.token} ${node.token > 1 ? 'tokens' : 'token'} left`
+    
+            if (latestTokenAmount != null) {
+                message = message.concat(`\n\n${parseInt(latestTokenAmount) - parseInt(node.token)} total tokens used over the previous 24 hours`)
+            }
+
+            fs.writeFile(filePath, node.token.toString(), (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(`New latest token amount saved to file named ${fileName} | ${node.token}`);
+            });
+
+            notifty.lineNotify(message);
+
         });
 
     } catch (err) {
@@ -54,17 +116,23 @@ async function queryNodeByTokenAmount(node_id_list) {
     try {
 
         let queriedNode = node_id_list.map(async (node) => {
+
+            if (config.UNCONDITIONAL_NODE_LIST.split(',').includes(node)) {
+                return undefined;
+            }
+
             const result = await tendermint.query('GetNodeToken', { node_id: node });
             const node_info = await getNodeInfo(node);
-            const unconditional_node_list = config.UNCONDITIONAL_NODE_LIST.split(',');
-            if (unconditional_node_list.includes(node)){
-                return {
-                    node_id: node,
-                    token: result.amount,
-                    node_name: node_info.marketing_name_en,
-                    role: node_info.role
-                }
-            }
+            // const unconditional_node_list = config.UNCONDITIONAL_NODE_LIST.split(',');
+            // if (unconditional_node_list.includes(node)){
+            //     return {
+            //         node_id: node,
+            //         token: result.amount,
+            //         node_name: node_info.marketing_name_en,
+            //         role: node_info.role
+            //     }
+            // }
+
             if (result.amount < config.TOKEN_THRESHOLD_TO_ALERT) {
                 return {
                     node_id: node,
